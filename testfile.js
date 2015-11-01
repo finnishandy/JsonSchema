801f015abd5cc1b8920154ddcd774c9edef65d2d
Finnishandy <finnishnandy@gmail.com>
2015-11-01 15:51:47 +0000

    froo

---
+++ testfile.js
@@ -0,0 +1,16 @@
+/**
+ * Created by Finnishandy on 01/11/2015.
+ */
+var webdriver = require('selenium-webdriver'),
+    By = require('selenium-webdriver').By,
+    until = require('selenium-webdriver').until;
+
+var driver = new webdriver.Builder()
+    .forBrowser('chrome')
+    .build();
+
+driver.get('http://www.google.com/ncr');
+driver.findElement(By.name('q')).sendKeys('webdriver');
+driver.findElement(By.name('btnG')).click();
+driver.wait(until.titleIs('webdriver - Google Search'), 5000);
+driver.quit();
